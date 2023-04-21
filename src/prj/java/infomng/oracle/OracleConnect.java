29ffd7f15f129afe5aacdb247a5282e1f6b05948
kkwh <121307297+kkwh@users.noreply.github.com>
2023-04-21 19:50:15 +0900

    Delete src/prj/java/infomng directory

--- src/prj/java/infomng/oracle/OracleConnect.java
+++
@@ -1,9 +0,0 @@
-package prj.java.infomng.oracle;
-
-public interface OracleConnect {
-	String URL = "jdbc:oracle:thin:@localhost:1521:xe";
-	
-	String USER = "scott";
-	
-	String PASSWORD = "tiger";
-}
